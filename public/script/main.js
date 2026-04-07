f1d32132b21f35bed0c3bbf07b3457a2126d9569
maxim_sinko <maximit705@gmail.com>
2026-04-07 17:51:49 +0300

    categories

--- public/script/main.js
+++ public/script/main.js
@@ -1,12 +1,16 @@
 const API_URL = 'https://fakestoreapi.com/';
-const newProductsContainer = document.querySelector('.new-products-container')
+const newProductsContainer = document.querySelector('.new-products-container');
+const mensContainer = document.querySelector('.mens-container');
+const womensContainer = document.querySelector('.womens-container');
+const electronicsContainer = document.querySelector('.electronics-container');
+const jewelryContainer = document.querySelector('.jewelry-container');
 
 
 
 function renderCard(product, container) {
 
     let divEl = document.createElement('div');
-    divEl.classList.add('card', 'product-card', 'col-md-3', );
+    divEl.classList.add('card', 'product-card', 'col-md-3',);
     divEl.innerHTML = `
  <img src="${product.image}"
      class="card-img-top">
@@ -14,6 +18,9 @@ function renderCard(product, container) {
         <h5 class="card-title">${product.title}</h5>
         <p class="price">$${product.price}</p>
     </div>
+
+
+    
     <a href="product.html?id=${product.id}" class="btn btn-primary view">View Product<a>
 `;
     container.appendChild(divEl)
@@ -29,8 +36,8 @@ const fetchProducts = async () => {
 
 
 
-    for (let i = 0; i < data.length; i++) {
-        let random = Math.floor(Math.random() * 19) + 1;
+    for (let i = 0; i < 4; i++) {
+        let random = Math.floor(Math.random() * data.length);
         console.log(random);
         data.forEach(product => {
             if (product.id === random) {
@@ -42,7 +49,125 @@ const fetchProducts = async () => {
     }
 
 
-}
+
+  
+    const womensResponse = await fetch(API_URL + "products/category/women's clothing");
+    const womensData = await womensResponse.json();
+    womensContainer.innerHTML = '';
+
+        womensData.forEach(product => {
+                renderCard(product, womensContainer);
+        
+        });
+  
+
+
+  
+    const mensResponse = await fetch(API_URL + "products/category/men's clothing");
+    const mensData = await mensResponse.json();
+    mensContainer.innerHTML = '';
+
+    
+
+        mensData.forEach(product => {
+           
+                renderCard(product, mensContainer);
+         
+        });
+    
+
+
+ 
+    const electronicsResponse = await fetch(API_URL + "products/category/electronics");
+    const electronicsData = await electronicsResponse.json();
+    electronicsContainer.innerHTML = '';
+
+  
+
+        electronicsData.forEach(product => {
+            
+                renderCard(product, electronicsContainer);
+          
+        });
+ 
+
+
+  
+    const jewelryResponse = await fetch(API_URL + "products/category/jewelery");
+    const jewelryData = await jewelryResponse.json();
+    jewelryContainer.innerHTML = '';
+
+    
+
+        jewelryData.forEach(product => {
+           
+                renderCard(product, jewelryContainer);
+            
+        });
+    }
+
+
+
 fetchProducts();
 
 
+
+
+
+
+
+
+
+// const BASE_API_URL = "https://dummyjson.com/";
+// const reviewsContainer = document.querySelector('.reviews-container')
+
+// const fetchReview = async (productId) => {
+//     try {
+//         const response = await fetch(`${BASE_API_URL}comments?limit=4&skip=${productId * 10}`);
+//         const data = await response.json();
+//         reviewsContainer.innerHTML = '';
+//         data.comments.forEach(review => {
+//             // console.log(review);
+//             renderReview(review);
+
+//         })
+
+//         console.log(data.comments);
+
+//     } catch (err) {
+//         console.log(err);
+//     }
+
+// }
+// fetchReview(+productId);
+
+// function renderReview(review) {
+//     const reviewCard = document.createElement('div');
+//     // renderCard.classList.add( тут мають бути класи);
+//     reviewCard.innerHTML = `
+    
+//     // контейнер, відображати на сторінці ${review.user.fullname},${review.body},
+//     // ${likeReview { review.rating }
+// }
+
+
+// `
+// reviewsContainer.appendChild('reviewCard');
+
+// }
+
+// function likeReview(likes){
+
+//     const likesBox = document.createElement('div');
+//     // likesBox.classList.add(мають бути класи)
+//     for(let i = 1; i <= likes; i++){
+//        likesBox.innerHTML = `
+//     //    зображення зірки
+//     `
+//     }
+//     console.log(likesBox);
+
+
+//     return likesBox.outerHTML;
+
+// }
